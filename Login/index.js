096effd2e945767f23416354a24402c3369f31d5
enzouribe <enzo.uribe@gmail.com>
2020-01-19 13:28:10 -0300

    Actualiza index.js

--- Login/index.js
+++ Login/index.js
@@ -5,20 +5,21 @@ function ingresarDatos(){
     const input1= window.document.querySelector('#email');
     const input2 = window.document.querySelector('#contraseña');
     const textoInput = input1.value;
-    const textoInput2 = input2.value;
+    const textoInput2 = input2.value;    
 
     if (textoInput === 'ejemplo@gmail.com' && textoInput2 === '123456'){
-        console.log('Iniciando sesión...')    
-    } 
+        document.getElementById('mensaje').innerHTML = 'Iniciando sesión…';
+        document.location.assign('../Layout/index.html');          
+    }  
     else {
-        console.log('Email y/o contraseña incorrectos')
+        
+        document.getElementById("mensaje").innerHTML = "Nombre de usuario y/o contraseña incorrectos";
     }
-
-    input1.value = '';
+    /*input1.value = '';
     input1.focus(); 
-
     input2.value = '';
-    input2.focus(); 
+    input2.focus(); */
+    
 }
 
 
